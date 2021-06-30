5340187e7089260ceb648e4d64f7dc469d56d212
rajbhowmick27 <52496816+rajbhowmick27@users.noreply.github.com>
2021-07-01 02:42:29 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const request = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params: {
-        key: "AIzaSyDTK_MaQclKjnKav7oc4Pc3vxW2WhICfOc",
+        key: 'AIzaSyDTK_MaQclKjnKav7oc4Pc3vxW2WhICfOc',
     }
 })
 
