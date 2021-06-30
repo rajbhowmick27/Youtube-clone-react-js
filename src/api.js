da1a581a2f4de084d1c6a3347accaec3f6865180
Raj Bhowmick <rb941849@student.nitw.ac.in>
2021-07-01 02:36:50 +0530

    api file changed

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const request = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params: {
-        key: process.env.REACT_APP_YT_API_KEY,
+        key: "AIzaSyDTK_MaQclKjnKav7oc4Pc3vxW2WhICfOc",
     }
 })
 
